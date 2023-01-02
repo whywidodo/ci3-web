bfb5edc1526f5ce4c6e42c937836842c93e0d65d
whywidodo <mr.wahyuwidodo@gmail.com>
2023-01-02 21:32:13 +0700

    update database

--- db_news.sql
+++ db_news.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 27 Des 2022 pada 02.33
--- Versi server: 10.4.22-MariaDB
--- Versi PHP: 8.1.2
+-- Generation Time: Jan 02, 2023 at 03:31 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_article`
+-- Table structure for table `tb_article`
 --
 
 CREATE TABLE `tb_article` (
@@ -37,20 +37,47 @@ CREATE TABLE `tb_article` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_article`
+-- Dumping data for table `tb_article`
 --
 
 INSERT INTO `tb_article` (`id`, `title`, `slug`, `content`, `draft`, `created_at`) VALUES
 ('1', 'Aksi Senyap Fund Manager Asing Borong Saham GoTo Saat Jatuh', 'manger-asing-borong-saham-goto', 'Aksi Senyap Fund Manager Asing Borong Saham GoTo Saat Jatuh,Tren pelemahan harga saham PT GoTo Gojek Tokopedia Tbk (GoTo) tidak menyurutkan minat investor untuk melakukan akasi borong. Sejak periode lock-up dibuka per awal Desember 2022, harga saham GOTO sudah anjlok lebih dari 50%. Namun beberapa fund manager asing justru memanfaatkan momentum tersebut untuk \\\'tambah muatan\\\'. Mengacu pada data Refinitiv Datastream ada beberapa nama investor asing yang membeli saham GOTO sejak akhir Oktober 2022', 'false', '2022-12-23 08:11:05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_feedback`
+--
+
+CREATE TABLE `tb_feedback` (
+  `id` varchar(32) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `email` varchar(32) NOT NULL,
+  `message` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_feedback`
+--
+
+INSERT INTO `tb_feedback` (`id`, `name`, `email`, `message`, `created_at`) VALUES
+('63b2ea59bd4273.43347990', 'Wahyu Widodo', 'wahyu@gmail.com', 'Cek pesan contact', '2023-01-02 14:29:45');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `tb_article`
+-- Indexes for table `tb_article`
 --
 ALTER TABLE `tb_article`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_feedback`
+--
+ALTER TABLE `tb_feedback`
   ADD PRIMARY KEY (`id`);
 COMMIT;
 
